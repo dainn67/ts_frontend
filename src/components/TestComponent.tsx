9fd00fdc42d13d281a60cdfcee9b72ef6d8faa17
Đại Nguyễn <dainguyen@ais-Mac-mini.local>
2024-11-29 17:44:23 +0700

    test

---
+++ src/components/TestComponent.tsx
@@ -0,0 +1,111 @@
+import { act, LegacyRef, memo, useMemo, useReducer, useRef, useState } from "react";
+
+interface JobState {
+    currentJob: string,
+    jobs: string[]
+}
+
+// Initstate
+const initState: JobState = {
+    currentJob: '',
+    jobs: []
+}
+
+// Action Types
+const SET_JOB = "set_job";
+const ADD_JOB = "add_job";
+const DELETE_JOB = "delete_job";
+
+interface SetJobAction {
+    type: typeof SET_JOB
+    payload: string
+}
+
+interface AddJobAction {
+    type: typeof ADD_JOB
+    payload: string
+}
+
+interface DeleteJobAction {
+    type: typeof DELETE_JOB
+    payload: number
+}
+
+type JobAction = SetJobAction | AddJobAction | DeleteJobAction
+
+// Action Creators
+const setJob = (payload: string): SetJobAction => ({
+    type: SET_JOB,
+    payload
+});
+
+const addJob = (payload: string): AddJobAction => ({
+    type: ADD_JOB,
+    payload
+})
+
+const deleteJob = (payload: number): DeleteJobAction => ({
+    type: DELETE_JOB,
+    payload
+});
+// Reducer
+const reducer = (state: JobState, action: JobAction): JobState => {
+    switch (action.type) {
+        case SET_JOB: return {
+            ...state,
+            currentJob: action.payload
+        }
+        case ADD_JOB: return {
+            ...state,
+            currentJob: '',
+            jobs: [...state.jobs, action.payload]
+        }
+        case DELETE_JOB: return {
+            ...state,
+            jobs: state.jobs.filter((_: string, index: number) => index !== action.payload)
+        }
+        default: throw new Error('Invalid action')
+    }
+
+    return state
+}
+
+// Dispatch: trigger reducer with action
+
+const TestComponent: React.FC = () => {
+    const [state, dispatch] = useReducer(reducer, initState)
+
+    const { currentJob, jobs } = state
+
+    const _handleAdd = () => {
+        dispatch(addJob(currentJob))
+        inputRef.current?.focus()
+    }
+
+    const inputRef = useRef<HTMLInputElement>(null)
+
+    return (
+        <div style={{ textAlign: "center", marginTop: "20px" }}>
+            <h1>Test Component</h1>
+            <br />
+            <input
+                type="text"
+                value={currentJob}
+                ref={inputRef}
+                onChange={e => dispatch(setJob(e.target.value))}
+                placeholder="Enter job"
+            />
+            <button onClick={_handleAdd}>Add</button>
+
+            <br />
+            <ul>
+                {jobs.map((job: string, index: number) => <li
+                    key={index}
+                    onClick={() => dispatch(deleteJob(index))}
+                >{job}</li>)}
+            </ul>
+        </div>
+    );
+};
+
+export default memo(TestComponent);
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,20 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+const emmitComments = (id: number) => {
+  setInterval(() => {
+    window.dispatchEvent(
+      new CustomEvent(`lesson-${id}`, {
+        detail: `Lession ${id}'s content`
+      })
+    )
+  }, 2000)
+}
+
+emmitComments(1)
+emmitComments(2)
+emmitComments(3)
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
